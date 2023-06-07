aa5387cc94538c1503eac588d1c29e2c2d6b74bc
Vatsal <vatsaljaganwala27@gmail.com>
2023-06-08 03:17:06 +0530

    NAVIGATION BAR CREATED

--- app/src/main/java/com/example/movix/MainActivity.java
+++ app/src/main/java/com/example/movix/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.movix;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView nav;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nav = findViewById(R.id.nav);
+
+
+
     }
 }
