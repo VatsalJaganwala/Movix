3a2c84d1cb1e3855339836a7400d1c54beafa285
Vatsal <vatsaljaganwala27@gmail.com>
2023-06-19 02:08:29 +0530

    INTENT OPTIMISEDD FOR CustomAdapter

--- app/src/main/java/com/example/movix/CustomAdapter.java
+++ app/src/main/java/com/example/movix/CustomAdapter.java
@@ -29,10 +29,21 @@ import java.util.ArrayList;
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
 
 //    private String[] movieNames;
-    private ArrayList<String> movieNames = new ArrayList<>();
+    private ArrayList<String> name = new ArrayList<>();
     private ArrayList<String> posterUrl = new ArrayList<>();
-    private ArrayList<Integer> movieId = new ArrayList<>();
+    private ArrayList<String> backdropPathList = new ArrayList<>();
+    private ArrayList<ArrayList<Integer>> genreIdsList = new ArrayList<>();
+    private ArrayList<Integer> idList = new ArrayList<>();
+    private ArrayList<String> originalLanguageList = new ArrayList<>();
+    private ArrayList<String> overviewList = new ArrayList<>();
+    private ArrayList<String> releaseDateList = new ArrayList<>();
+    private ArrayList<Double> voteAverageList = new ArrayList<>();
+    private ArrayList<Integer> voteCountList = new ArrayList<>();
     private JSONArray movieDetails;
+
+    private ArrayList<Boolean> isMovie = new ArrayList<>();
+
+
 
     /**
      * Provide a reference to the type of views that you are using
@@ -66,13 +77,36 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
         for (int i = 0; i < jsonArray.length(); i++) {
             JSONObject explrObject = jsonArray.getJSONObject(i);
             if(explrObject.has("title")) {
-                this.movieNames.add(explrObject.getString("title"));
+                this.name.add(explrObject.getString("title"));
+//                this.releaseDateList.add(explrObject.getString("release_date"));
+                isMovie.add(true);
             }
             else {
-            this.movieNames.add(explrObject.getString("name"));
+            this.name.add(explrObject.getString("name"));
+//            this.releaseDateList.add(explrObject.getString("first_air_date"));
+                isMovie.add(false);
             }
             this.posterUrl.add(explrObject.getString("poster_path"));
-            this.movieId.add(explrObject.getInt("id"));
+            this.idList.add(explrObject.getInt("id"));
+//            this.backdropPathList.add(explrObject.getString("backdrop_path"));
+//            try{
+//                JSONArray genreIdsArray = explrObject.getJSONArray("genre_ids");
+//                ArrayList<Integer> genre = new ArrayList<>();
+//                for (int j = 0; j < genreIdsArray.length(); j++) {
+//                    genre.add(genreIdsArray.getInt(i));
+//                }
+//                genreIdsList.add(genre);
+//            }
+//            catch (JSONException e){
+//                e.printStackTrace();
+//                Log.e("Genre Failed", "CustomAdapter: "+ name.get(name.size()-1) );
+//
+//            }
+//            originalLanguageList.add(explrObject.getString("original_language"));
+//            overviewList.add(explrObject.getString("overview"));
+//            voteAverageList.add(explrObject.getDouble("vote_average"));
+//            voteCountList.add(explrObject.getInt("vote_count"));
+
 
         }
     }
@@ -88,18 +122,22 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
-
+    public void onBindViewHolder(ViewHolder viewHolder, final int index) {
+        int position = index;
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
-        viewHolder.getTextView().setText(movieNames.get(position));
+        viewHolder.getTextView().setText(name.get(position));
         String url ="https://image.tmdb.org/t/p/w185"+ posterUrl.get(position) ;
         Picasso.get().load(url).into(viewHolder.getMovieposterView());
         viewHolder.getTextView().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(v.getContext(), movieNames.get(position), Toast.LENGTH_SHORT).show();
+                Toast.makeText(v.getContext(), name.get(position), Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(v.getContext(),MovieDetails.class);
+                intent.putExtra("id", idList.get(position));
+                intent.putExtra("ISMOVIE", isMovie.get(position));
+
+
                 v.getContext().startActivity(intent);
             }
         });
@@ -112,7 +150,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return movieNames.size();
+        return name.size();
     }
 
 }
--- app/src/main/java/com/example/movix/MovieDetails.java
+++ app/src/main/java/com/example/movix/MovieDetails.java
@@ -3,13 +3,197 @@ package com.example.movix;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MovieDetails extends AppCompatActivity {
-
+        String nameValue ,posterUrlValue ,backdropPathValue  ,originalLanguageValue,overviewValue,releaseDateValue,url1;
+        int idValue;
+    double voteAverageValue;
+    int voteCountValue ;
+    JSONObject moviejson;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy gfgPolicy =
+                    new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(gfgPolicy);
+        }
+
+
+        idValue = getIntent().getIntExtra("id", 0);
+        Boolean isMovie = getIntent().getBooleanExtra("ISMOVIE",true);
+        ArrayList<Integer> genreIdsValue;
+
+        if (isMovie){
+            url1 = "https://api.themoviedb.org/3/movie/" + idValue + "/external_ids";
+        }
+        else {
+            url1 = "https://api.themoviedb.org/3/tv/" + idValue + "/external_ids";
+        }
+        Toast.makeText(this, url1, Toast.LENGTH_SHORT).show();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,url1,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Handle the response here
+                        Log.d("Response", response);
+                        String jsonString =response;
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(jsonString);
+                            String imdbId = jsonObject.getString("imdb_id");
+                            getDetails(imdbId);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle the error here
+                        Log.e("Error", error.toString());
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("accept", "application/json");
+                headers.put("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZGM4OTUzZTU5NGI5ODUyZDAzYTQxMjI1ZWNmYTU2MCIsInN1YiI6IjY0ODA3N2VhOTkyNTljMDBhY2NhOWVmYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4Y2qHnJxT3zXsV7eMvQl3jYlIGaY8FZEGuzB2NHsZMM");
+                return headers;
+            }
+        };
+        // Add the request to the RequestQueue
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+
+    }
+    private void getDetails(String imdbId){
+        String url = "https://api.themoviedb.org/3/find/"+imdbId+"?external_source=imdb_id";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Handle the response here
+                        JSONObject explrObject = new JSONObject();
+                        Log.d("Response", response);
+                        String jsonString = response;
+
+                        // Parse the JSON string
+                        JSONObject jsonObject = null;
+                        try {
+                            jsonObject = new JSONObject(jsonString);
+
+                        // Extract data from movie_results or tv_results
+                        JSONArray movieResults = jsonObject.getJSONArray("movie_results");
+                        JSONArray tvResults = jsonObject.getJSONArray("tv_results");
+
+                        if (movieResults.length() > 0) {
+
+                            explrObject = movieResults.getJSONObject(0);
+
+                        } else if (tvResults.length() > 0) {
+                            explrObject = tvResults.getJSONObject(0);
+                        }
+                        setData(explrObject);
+
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle the error here
+                        Log.e("Error", error.toString());
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("accept", "application/json");
+                headers.put("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZGM4OTUzZTU5NGI5ODUyZDAzYTQxMjI1ZWNmYTU2MCIsInN1YiI6IjY0ODA3N2VhOTkyNTljMDBhY2NhOWVmYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4Y2qHnJxT3zXsV7eMvQl3jYlIGaY8FZEGuzB2NHsZMM");
+                return headers;
+            }
+        };
+        // Add the request to the RequestQueue
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+    private void setData(JSONObject explrObject) throws JSONException {
+
+        if(explrObject.has("title")) {
+            nameValue=explrObject.getString("title");
+
+        }
+        else {
+            nameValue=explrObject.getString("name");
+        }
+        posterUrlValue=explrObject.getString("poster_path");
+        idValue=explrObject.getInt("id");
+            backdropPathValue=explrObject.getString("backdrop_path");
+//            try{
+//                JSONArray genreIdsArray = explrObject.getJSONArray("genre_ids");
+//                ArrayList<Integer> genre = new ArrayList<>();
+//                for (int j = 0; j < genreIdsArray.length(); j++) {
+//                    genre.add(genreIdsArray.getInt(i));
+//                }
+//                genreIdsList.add(genre);
+//            }
+//            catch (JSONException e){
+//                e.printStackTrace();
+//                Log.e("Genre Failed", "CustomAdapter: "+ name.get(name.size()-1) );
+//
+//            }
+            originalLanguageValue=explrObject.getString("original_language");
+            overviewValue=explrObject.getString("overview");
+            voteAverageValue= explrObject.getDouble("vote_average");
+            voteCountValue=explrObject.getInt("vote_count");
+        TextView name = findViewById(R.id.name);
+        name.setText(nameValue);
+
+    }
+
+    private JSONArray getJSONArray(String response){
+        JSONArray jsonArray = null;
+        try {
+            JSONObject jsnobject = new JSONObject(response);
+            jsonArray = jsnobject.getJSONArray("results");
+            return jsonArray;
+
+        }catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return jsonArray;
 
     }
 }
