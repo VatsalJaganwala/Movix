6f6898286ff91ed4e35256ff7f228b22daf85054
Vatsal <vatsaljaganwala27@gmail.com>
2023-06-25 15:17:18 +0530

    Version 1(Minor Update)

--- app/src/main/java/com/example/movix/MainActivity.java
+++ app/src/main/java/com/example/movix/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.annotation.SuppressLint;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -20,6 +22,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         nav = findViewById(R.id.nav);
+        ImageView logo = findViewById(R.id.logo);
+        int nightModeFlags =
+                this.getResources().getConfiguration().uiMode &
+                        Configuration.UI_MODE_NIGHT_MASK;
+        switch (nightModeFlags) {
+            case Configuration.UI_MODE_NIGHT_YES:
+                logo.setImageResource(R.drawable.logo_white);
+                break;
+
+            case Configuration.UI_MODE_NIGHT_NO:
+                logo.setImageResource(R.drawable.logo_black);
+                break;
+
+            case Configuration.UI_MODE_NIGHT_UNDEFINED:
+                logo.setImageResource(R.drawable.logo_black);
+                break;
+        }
         nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
